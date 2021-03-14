9b7c8c0fd97278654b41fd4ff59bb5775f339853
Anton Gavrish <tosh.the@yandex.ru>
2021-03-14 12:36:33 +0300

    base classes for Transport

---
+++ main.py
@@ -0,0 +1,32 @@
+class Transport:
+	def __init__ (self, fuel):
+		self.fuel = fuel
+		self.trips = []
+
+	def add_trip (self, trip):
+		self.trips.append(trip)
+
+	def sum_trips_distance (self):
+		return sum(trip.distance for trip in self.trips)
+
+	def calculate_reachable_distance (self):
+		raise NotImplementedError
+
+class Car (Transport):
+	FUEL_CONSUMPTION_CAR = 0.12
+
+	def calculate_reachable_distance (self):
+		distance_covered = self.sum_trips_distance()
+		result = (self.fuel - (distance_covered * self.FUEL_CONSUMPTION_CAR)) // self.FUEL_CONSUMPTION_CAR
+		return f"Топлива осталось на {result} км."
+
+class Airplane (Transport):
+	FUEL_CONSUMPTION_AIRPLANE = 200
+
+	def calculate_reachable_distance (self):
+		distance_covered = self.sum_trips_distance()
+		result = (self.fuel - (distance_covered * self.FUEL_CONSUMPTION_AIRPLANE)) // self.FUEL_CONSUMPTION_AIRPLANE
+		return f"Топлива остлаось на {result} ч. полета"
+
+c = Car(50)
+print(c.calculate_reachable_distance())
